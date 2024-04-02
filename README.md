0b83a02a1eef37f8dea28846cf53e8f0931231ff
Brian <brianmazini@gmail.com>
2024-04-01 22:45:27 -0300

    tailwind + lucid icon

---
+++ README.md
@@ -0,0 +1,5 @@
+# Pass-in-Web
+
+## Instruções
+
+- https://efficient-sloth-d85.notion.site/React-529e4443080a4d889f7a0714aa09348c
---
+++ src/components/header.tsx
@@ -0,0 +1,18 @@
+import nlwUniteIcon from "../assets/nlw-unite-icon.svg";
+
+export function Header() {
+  return (
+    <div className="flex items-center gap-5">
+      <img src={nlwUniteIcon}></img>
+
+      <nav className="flex items-center gap-5 p-2">
+        <a href="" className="font-medium text-sm text-zinc-300">
+          Eventos
+        </a>
+        <a href="" className="font-medium text-sm">
+          Participantes
+        </a>
+      </nav>
+    </div>
+  );
+}
