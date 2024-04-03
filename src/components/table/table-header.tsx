cb5abac914e2b8e82733d72a5c70641a60cc8dda
Brian <brianmazini@gmail.com>
2024-04-03 20:44:21 -0300

    aula 2 finalizada

--- src/components/table/table-header.tsx
+++ src/components/table/table-header.tsx
@@ -3,9 +3,5 @@ import { ComponentProps } from "react";
 interface TableHeaderProps extends ComponentProps<"th"> {}
 
 export function TableHeader(props: TableHeaderProps) {
-  return (
-    <th className="py3 px-4 text-sm font-semibold text-left">
-      {props.children}
-    </th>
-  );
+  return <th className="py3 px-4 text-sm font-semibold text-left" {...props} />;
 }
--- src/components/table/table.tsx
+++ src/components/table/table.tsx
@@ -5,7 +5,7 @@ interface TableProps extends ComponentProps<"table"> {}
 export function Table(props: TableProps) {
   return (
     <div className="border border-white/10 rounded-lg">
-      <table className="w-full">{props.children}</table>
+      <table className="w-full" {...props} />
     </div>
   );
 }
